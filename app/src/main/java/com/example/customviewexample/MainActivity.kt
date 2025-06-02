dc88e31d6064fa03bd8a557e7d0bcb773efcb224
Mawv1 <mateuszwasiak81@interia.pl>
2025-06-02 22:30:41 +0200

    Added button ot change screen

--- app/src/main/java/com/example/customviewexample/MainActivity.kt
+++ app/src/main/java/com/example/customviewexample/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.customviewexample
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,14 @@ class MainActivity : AppCompatActivity() {
 
         val barsWaveView = findViewById<BarsWaveView>(R.id.bars_wave_view)
         val playStopButton = findViewById<AnimatedPlayStopButton>(R.id.play_stop_button)
+
+        val nextScreenButton = findViewById<Button>(R.id.next_screen_button)
+
+        nextScreenButton.setOnClickListener {
+            val intent = Intent(this, SecondScreen::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
 
         playStopButton.setOnClickListener {
             if (barsWaveView.isRunning) {
